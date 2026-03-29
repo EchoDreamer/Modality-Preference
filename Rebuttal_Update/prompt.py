1513b0b5197c010e41ca0ad84153faf2c0deb127
Yu Zhang（张宇） <115880161+EchoDreamer@users.noreply.github.com>
2026-03-29 17:39:53 +0800

    Add files via upload

---
+++ Rebuttal_Update/prompt.py
@@ -0,0 +1,13 @@
+# paper use:'
+instruction_text = "In case there is an inconsistency between the Text Context and the input image content, you must rely on the text context rather than the input image content. "
+instruction_image = "In case there is an inconsistency between the text context and the image content, you should follow the image content rather than the text context. "
+
+
+# version 1
+instruction_text = "In case there is an inconsistency between the text context and the input image content, prioritize the information from the text context. "
+instruction_image = "In case there is an inconsistency between the text context and the image content, you should follow the image content rather than the text context. "
+
+
+# version 2
+instruction_text = "In case there is an inconsistency between the text context and the image content, you should follow the text context rather than the image content. "
+instruction_image = "In case there is an inconsistency between the text context and the image content, you should follow the image content rather than the text context. "
